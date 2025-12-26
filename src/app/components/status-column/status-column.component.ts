72acc8fd4092ce5fd118ffd7d8d47f0aeede6ca3
Parth Tyagi <parthtyagicode@gmail.com>
2025-12-26 15:48:36 +0530

    Completed the structure of the project

--- src/app/components/status-column/status-column.component.ts
+++ src/app/components/status-column/status-column.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-status-column',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './status-column.component.html',
   styleUrl: './status-column.component.scss'
 })
 export class StatusColumnComponent {
+
+
+  
 
 }
